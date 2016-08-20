9df8187dae0432fd877e4ae6f0371b50fd90ecb7
Nick Shvelidze <captain@pirrate.me>
2016-08-20 23:52:43 +0400

    Randomly refuse service

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 var express = require('express'),
 	handler = require('./lib/handler'),
 	setHeaders = require('./lib/setHeaders'),
+	randomlyUnavailable = require('./lib/randomlyUnavailable'),
 	DEFAULT_PORT = 8008,
 	port = process.env.PORT || DEFAULT_PORT;
 
 var app = express();
 
 app.use(setHeaders);
+app.use(randomlyUnavailable);
 app.get('/', handler);
 
 app.listen(port, function () {
